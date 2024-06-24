5f20b3e5e7ae43c5c9bf1a18ebf11619b43087cd
Ewurabena08 <estherawuah44@gmail.com>
2024-06-24 19:39:25 +0000

    updated

--- 0x00-ES6_basic/babel.config.js
+++ 0x00-ES6_basic/babel.config.js
@@ -1,11 +1,12 @@
-module.exports = function (api) {
-  api.cache(true);
-
-  const presets = ['@babel/preset-env'];
-  // Add more presets, plugins, or other configurations here as needed
-
-  return {
-    presets
-  };
-}
-
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
